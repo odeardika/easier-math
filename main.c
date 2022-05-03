8faee9ed8da98365fb83c5559aa7148232f13d70
odeardika <dirfanardika@gmail.com>
2022-05-03 21:35:41 +0800

    beta kalkulator

--- main.c
+++ main.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 
+double kalkulator(){
+    char pil;
+    double awal, penambah;
+    printf("Masukan Angka Pertama : ");
+    scanf("%lf", &awal);
+    fflush(stdin);
+    printf("Masukan Pilihan ( + - * / ) : ");
+    scanf("%c", &pil);
+    printf("Masukan Angka Penambah : ");
+    scanf("%lf", &penambah);
+
+    if(pil == '+') return awal + penambah;
+    if(pil == '-') return awal - penambah;
+    if(pil == '*') return awal * penambah;
+    if(pil == '/') return awal / penambah;
+}
+
 int main(){
+    double hasil;
     printf("Menu :\n"
            "1. Kalkulator\n"
            "2. Bangun Datar\n"
            "3. Bangun Ruang\n");
     char pilihan;
     printf("Masukan Pilihan : ");
-    scanf("%c");
+    scanf("%c", &pilihan);
     switch (pilihan)
     {
     case '1':
-        //kalkulator
+        hasil = kalkulator();
+        printf("Hasil : %.2lf", hasil);
         break;
     case '2':
         //Bangun Datar
@@ -22,4 +41,6 @@ int main(){
     default:
         break;
     }
+    return 0;
 }
+
