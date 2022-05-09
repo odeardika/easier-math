8abec74c8b3f16fdbedc55edd2a406ef38df8d35
odeardika <dirfanardika@gmail.com>
2022-05-09 23:54:08 +0700

    rumus persegi

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 double kalkulator();
 void MBDatar(); //Menu Bangun Datar
@@ -13,6 +14,7 @@ int main(){
     char pilihan;
     printf("Masukan Pilihan : ");
     scanf("%c", &pilihan);
+    while(getchar() != '\n');
     switch (pilihan)
     {
     case '1':
@@ -36,7 +38,6 @@ double kalkulator(){
     double awal, penambah;
     printf("Masukan Angka Pertama : ");
     scanf("%lf", &awal);
-    while(getchar() != '\n');
     printf("Masukan Pilihan ( + - * / ) : ");
     scanf("%c", &pil);
     printf("Masukan Angka Penambah : ");
@@ -49,6 +50,8 @@ double kalkulator(){
 }
 
 void MBDatar(){
+    char pilihan;
+    double sisi,luas,keliling;
     printf("Menu :\n"
            "1. Persegi\n"
            "2. Persegi Panjang\n"
@@ -57,7 +60,56 @@ void MBDatar(){
            "5. Trapesium\n"
            "6. Belah Ketupat\n"
            "7. Layang - layang\n"
-           "8. Lingkaran\n");
+           "8. Lingkaran\n"
+           "Masukan Pilihan : ");
+    scanf("%c",&pilihan);
+    while(getchar() != '\n');
+    switch (pilihan)
+    {
+    case '1':
+        char rumus;
+        printf("Menu Persegi\n"
+               "1. Mencari Luas\n"
+               "2. Mencari Keliling\n"
+               "3. Mencari Sisi (dengan luas)\n"
+               "4. Mencari Sisi (dengan keliling)\n"
+               "Masukan Pilihan : ");
+                scanf("%c", &rumus);
+                switch (rumus)
+                {
+                case '1':
+                    printf("Masukan Sisi = ");
+                    scanf("%lf", &sisi);
+                    luas = sisi * sisi;
+                    printf("Luas = %.2lf", luas);
+                    break;
+                case '2':
+                    printf("Masukan Sisi = ");
+                    scanf("%lf", &sisi);
+                    keliling = sisi * 4;
+                    printf("Keliling = %.2lf", keliling);
+                    break;
+                case '3':
+                    printf("Masukan Luas = ");
+                    scanf("%lf", &luas);
+                    sisi = sqrt(luas);
+                    printf("Sisi = %.2lf", sisi);
+                    break;
+                case '4':
+                    printf("Masukan Luas = ");
+                    scanf("%lf", &keliling);
+                    sisi = keliling / 4;
+                    printf("Sisi = %.2lf", sisi);
+                    break;
+                default:
+                    break;
+               }
+        break;
+    
+    default:
+        break;
+    }
+
 }
 
 void MBRuang(){
